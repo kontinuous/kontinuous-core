6e9974e1db5190e0ea3e0539b35cf04d3ae181ea
Alex Khamutov <khamutov@ailabs.ru>
2013-01-27 17:52:08 +0400

    add view resolver

---
+++ src/main/kotlin/ru/ailabs/kontinuous/view/ViewResolver.kt
@@ -0,0 +1,31 @@
+package ru.ailabs.kontinuous.view
+
+import org.apache.velocity.app.Velocity
+import org.apache.velocity.VelocityContext
+import java.io.StringWriter
+import java.util.Properties
+
+/**
+ * Alien Invaders Ltd.
+ * User: Aleksandr Khamutov
+ * Date: 1/27/13
+ * Time: 4:36 PM
+ */
+class ViewResolver {
+
+    fun resolveView(params: Map<String, *>, viewName: String) : String {
+        val props = Properties()
+        props.setProperty("resource.loader", "class")
+        props.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader")
+
+        Velocity.init(props)
+        val context = VelocityContext(params)
+
+        val template = Velocity.getTemplate("views/hello/index.vm");
+        val sw = StringWriter();
+
+        template?.merge( context, sw );
+
+        return sw.toString()
+    }
+}
---
+++ src/test/kotlin/ru/ailabs/kontinuous/tests/view/ViewResolverTest.kt
@@ -0,0 +1,19 @@
+package ru.ailabs.kontinuous.tests.view
+
+import ru.ailabs.kontinuous.view.ViewResolver
+import org.junit.Test
+
+/**
+ * Alien Invaders Ltd.
+ * User: Aleksandr Khamutov
+ * Date: 1/27/13
+ * Time: 4:40 PM
+ */
+
+class ViewResolverTest {
+    Test fun shouldReturnText() {
+        val viewResolver = ViewResolver()
+
+        println(viewResolver.resolveView(hashMapOf("name" to "Aleksandr Khamutov"), "testTemplate"))
+    }
+}
